b371b9dbcae917f72c217e1ea5e1d6deb669c629
P1ST-0N <luckylionia@gmail.com>
2023-08-12 20:01:57 +0300

    Update index.tsx
    added basepath="/..." in main index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
 
   root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/mini-market">
       <Provider store={store}>
         <App />
       </Provider>
